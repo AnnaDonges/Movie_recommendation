23fd0d7d936e2d4f7fef7615bddb336eb12092bb
AnnaDonges <dongesGHP@gmail.com>
2023-07-06 09:49:45 -0400

    reading both csv files

--- movieRecommendation.py
+++ movieRecommendation.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+path = "C:/Users/ad29203/Desktop/GHP/Movie_recommendation"
+budgetGenres = pd.read_csv(path + "/budgetGenres.csv")
+castCreditCrew = pd.read_csv(path + "/castCrewTitle.csv")
+
+budgetGenres.head()
+castCreditCrew.head()
