7a2716e7d3521582d9f9bc46e546b72b3f3bd895
AnnaDonges <dongesGHP@gmail.com>
2023-07-06 10:41:07 -0400

    key features identifyed and looped through

--- movieRecommendation.py
+++ movieRecommendation.py
@@ -1,8 +1,32 @@
 import pandas as pd
+from IPython.display import display
+from ast import literal_eval
 
+# Set the path for the output CSV file
+output_path = "C:/Users/ad29203/Desktop/GHP/Movie_recommendation/output.csv"
+
+# Import CSV files
 path = "C:/Users/ad29203/Desktop/GHP/Movie_recommendation"
 budgetGenres = pd.read_csv(path + "/budgetGenres.csv")
 castCreditCrew = pd.read_csv(path + "/castCrewTitle.csv")
 
-budgetGenres.head()
-castCreditCrew.head()
+#budgetGenres.head()
+#castCreditCrew.head()
+
+# Merge columns
+castCreditCrew.columns = ['id', 'title', 'cast', 'crew']
+budgetGenres = budgetGenres.merge(castCreditCrew, on='id')
+
+# Display the merged dataframe as a table
+#budgetGenres.head()
+
+# Save the merged dataframe to a CSV file
+#budgetGenres.to_csv(output_path, index=False)
+
+#loop through key features needed for identifying similar movies
+features = ['cast', 'crew', 'keywords', 'genres']
+
+for feature in features:
+    budgetGenres[feature] = budgetGenres[feature].apply(literal_eval)
+
+display(budgetGenres[features].head(10)) 
